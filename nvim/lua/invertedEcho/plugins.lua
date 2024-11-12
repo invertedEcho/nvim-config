e9ab142122e7ca444bb060b567f4b97c4d5b8924
Jakob Stechow <jakob.stechow@pm.me>
2024-11-12 21:01:15 +0100

    fix: show hidden files in oil

--- nvim/lua/invertedEcho/plugins.lua
+++ nvim/lua/invertedEcho/plugins.lua
@@ -238,10 +238,8 @@ require('lazy').setup({
     'stevearc/oil.nvim',
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
-    -- Optional dependencies
-    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+    opts = { view_options = { show_hidden = true } },
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
   },
 }, {
   ui = {
