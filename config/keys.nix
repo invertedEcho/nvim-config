feb315dca60c3ccdb9b25026abbee81d4f0985f7
Jakob Stechow <jakob.stechow@pm.me>
2025-05-17 12:17:34 +0200

    feat: some keybinds to make living inside vim easier

--- config/keys.nix
+++ config/keys.nix
@@ -1,5 +1,6 @@
 {
   keymaps = [
+    # Move cursor in insert mode
     {
       action = "<left>";
       key = "<C-h>";
@@ -19,6 +20,48 @@
       action = "<down>";
       key = "<C-j>";
       mode = "i";
+    }
+    # Make navigation between buffers easier
+    {
+      action = "<C-w>h";
+      key = "<C-h>";
+      mode = "n";
+    }
+    {
+      action = "<C-w>j";
+      key = "<C-j>";
+      mode = "n";
+    }
+    {
+      action = "<C-w>k";
+      key = "<C-k>";
+      mode = "n";
+    }
+    {
+      action = "<C-w>l";
+      key = "<C-l>";
+      mode = "n";
+    }
+    # Make resizing buffers easier
+    {
+      action = "<C-w>2-";
+      key = "<C-S-k";
+      mode = "n";
+    }
+    {
+      action = "<C-w>2+";
+      key = "<C-j>";
+      mode = "n";
+    }
+    {
+      action = "<C-w>5<";
+      key = "<C-S-h>";
+      mode = "n";
+    }
+    {
+      action = "<C-W>5>";
+      key = "<C-S-l>";
+      mode = "n";
     }
   ];
 }
