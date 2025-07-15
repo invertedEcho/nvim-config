e0a444df46062f9d377def1dba81be69804364ac
Jakob Stechow <jakob.stechow@pm.me>
2025-07-15 11:14:16 +0200

    feat: user-command to disable formatter

--- config/plugins/conform.nix
+++ config/plugins/conform.nix
@@ -8,9 +8,6 @@
         javascript = ["prettierd"];
         typescript = ["prettierd"];
         typescriptreact = ["prettierd"];
-        dart = [
-          "dart_format"
-        ];
         # to be used with the prettier php plugin -> requires setup in your php project
         php = ["prettierd"];
         nix = ["alejandra"];
@@ -23,5 +20,34 @@
         timeout_ms = 500;
       };
     };
+    luaConfig.post = ''
+      require("conform").setup({
+        format_on_save = function(bufnr)
+          -- Disable with a global or buffer-local variable
+          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+          end
+          return { timeout_ms = 500, lsp_format = "fallback" }
+        end,
+      })
+
+      vim.api.nvim_create_user_command("FormatDisable", function(args)
+        if args.bang then
+          -- FormatDisable! will disable formatting just for this buffer
+          vim.b.disable_autoformat = true
+        else
+          vim.g.disable_autoformat = true
+        end
+      end, {
+        desc = "Disable autoformat-on-save",
+        bang = true,
+      })
+      vim.api.nvim_create_user_command("FormatEnable", function()
+        vim.b.disable_autoformat = false
+        vim.g.disable_autoformat = false
+      end, {
+        desc = "Re-enable autoformat-on-save",
+      })
+    '';
   };
 }
