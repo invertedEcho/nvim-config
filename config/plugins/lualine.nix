a98b66e1fd626eb75c56f8c077ec6bba022a161b
Jakob Stechow <jakob.stechow@pm.me>
2025-08-03 10:58:29 +0200

    feat: customize lualin sections

--- config/plugins/lualine.nix
+++ config/plugins/lualine.nix
@@ -5,6 +5,15 @@
       options = {
         section_separators = "";
         component_separators = "";
+        theme = "gruvbox_dark";
+      };
+      sections = {
+        lualine_a = ["mode"];
+        lualine_b = ["branch" "diagnostics"];
+        lualine_c = ["filename"];
+        lualine_x = ["filetype"];
+        lualine_y = ["progress"];
+        lualine_z = ["location"];
       };
     };
   };
