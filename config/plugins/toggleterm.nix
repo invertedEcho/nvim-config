28441c4cf1f110a372e62b0dca572afb1538785b
Jakob Stechow <jakob.stechow@pm.me>
2025-08-08 21:34:02 +0200

    Revert "fix(toggleterm): just basic toggleterm, everything else happens in different tmux window anyways"
    This reverts commit b7a8344b92ddb5d2261dbc62b1cf3735d93f3c21.

--- config/plugins/toggleterm.nix
+++ config/plugins/toggleterm.nix
@@ -2,10 +2,15 @@
   plugins.toggleterm = {
     enable = true;
   };
+  # TODO: I want to be able to have lazygit float terminal open and then also be able to spawn the "normal terminal"
   keymaps = [
     {
       key = "<C-\\>";
       action = "<cmd>ToggleTerm<cr>";
+    }
+    {
+      key = "<leader>g";
+      action = "<cmd>TermExec cmd=\"lazygit\" direction=float<cr>";
     }
     {
       key = "<C-\\>";
