fbaeecf25dd15db4d99679cef8be57cf9bbfe396
Jakob Stechow <jakob.stechow@pm.me>
2025-08-10 11:46:27 +0200

    feat: add coq artifacts

--- lua/plugins/coq.lua
+++ lua/plugins/coq.lua
@@ -1,6 +1,9 @@
 return {
-	"ms-jpq/coq_nvim",
-	config = function()
-		vim.cmd("COQnow -s")
-	end,
+	{
+		"ms-jpq/coq_nvim",
+		config = function()
+			vim.cmd("COQnow -s")
+		end,
+	},
+	{ "ms-jpq/coq.artifacts" },
 }
