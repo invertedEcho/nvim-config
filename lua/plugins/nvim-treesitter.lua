859f3c8c71ac97f7273bd0eefabab3d450d6d3d9
Jakob Stechow <jakob.stechow@pm.me>
2025-08-10 12:07:35 +0200

    feat: nvim-treesitter

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,28 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"lua",
+				"tsx",
+				"javascript",
+				"typescript",
+				"css",
+				"json",
+				"toml",
+				"rust",
+				"comment",
+				"php",
+				"dart",
+				"yaml",
+				"python",
+			},
+			highlight = {
+				enable = true,
+			},
+		})
+	end,
+}
