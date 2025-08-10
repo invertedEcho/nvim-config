4c186db4179aa40cbe8a2f89928c127c6332d35d
Jakob Stechow <jakob.stechow@pm.me>
2025-08-10 12:07:46 +0200

    feat: enable rust_analzyer lsp and lsp keybindings

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -5,3 +5,11 @@ vim.lsp.config("luals", {
 })
 
 vim.lsp.enable("luals")
+
+vim.lsp.enable("rust_analyzer")
+
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gr", vim.lsp.buf.references)
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+vim.keymap.set("n", "ga", vim.lsp.buf.code_action)
+vim.keymap.set("n", "ge", vim.diagnostic.open_float)
