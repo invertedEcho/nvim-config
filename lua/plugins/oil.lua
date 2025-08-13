3cff7959967ded13cd0c166a9ce6d9ead1739df9
Jakob Stechow <jakob.stechow@pm.me>
2025-08-13 12:09:09 +0200

    feat(oil): show hidden files, delete to trash, skip confirm for simple edits

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,7 +2,14 @@ return {
 	"stevearc/oil.nvim",
 	---@module 'oil'
 	---@type oil.SetupOpts
-	opts = {},
+	opts = {
+		delete_to_trash = true,
+		skip_confirm_for_simple_edits = true,
+		watch_for_changes = true,
+		view_options = {
+			show_hidden = true,
+		},
+	},
 	dependencies = { { "echasnovski/mini.icons", opts = {} } },
 	lazy = false,
 }
