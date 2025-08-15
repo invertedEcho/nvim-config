e77122630e85e47f8a8e5d879017868caf8912f5
Jakob Stechow <jakob.stechow@pm.me>
2025-08-15 19:05:09 +0200

    fix(blink-cmp): disable completion when in dressing input

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -8,6 +8,12 @@ return {
 	---@module 'blink.cmp'
 	---@type blink.cmp.Config
 	opts = {
+		-- disables completion when in dressing input
+		enabled = function()
+			return not vim.list_contains({ "DressingInput" }, vim.bo.filetype)
+				and vim.bo.buftype ~= "prompt"
+				and vim.b.completion ~= false
+		end,
 		keymap = {
 			preset = "default",
 			["<C-j>"] = { "select_next" },
