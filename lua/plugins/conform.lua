7661991ae77fe495c7a8f0d355ba0e583057c905
Jakob Stechow <jakob.stechow@pm.me>
2025-10-21 10:49:05 +0200

    fix: enable lsp format again

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -12,7 +12,7 @@ return {
 				php = { "prettierd" },
 				nix = { "alejandra" },
 			},
-			format_on_save = { timeout_ms = 500 },
+			format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
 		})
 	end,
 }
