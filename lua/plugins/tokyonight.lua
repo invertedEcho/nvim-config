866c9d03ca9b6c89f411ef58f57e56f05b131426
Jakob Stechow <jakob.stechow@pm.me>
2026-02-12 10:38:12 +0100

    feat: enable curly underline again

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -5,18 +5,18 @@ return {
 	enabled = true,
 	config = function()
 		vim.cmd("colorscheme tokyonight-storm")
-		local diags = {
-			"DiagnosticUnderlineError",
-			"DiagnosticUnderlineWarn",
-			"DiagnosticUnderlineInfo",
-			"DiagnosticUnderlineHint",
-		}
+		-- local diags = {
+		-- 	"DiagnosticUnderlineError",
+		-- 	"DiagnosticUnderlineWarn",
+		-- 	"DiagnosticUnderlineInfo",
+		-- 	"DiagnosticUnderlineHint",
+		-- }
 
-		for _, group in ipairs(diags) do
-			vim.api.nvim_set_hl(0, group, {
-				undercurl = false,
-				underline = true,
-			})
-		end
+		-- for _, group in ipairs(diags) do
+		-- 	vim.api.nvim_set_hl(0, group, {
+		-- 		undercurl = false,
+		-- 		underline = true,
+		-- 	})
+		-- end
 	end,
 }
