299628fbb99a68c179121236a7eee55235ffc7fd
Jakob Stechow <jakob.stechow@pm.me>
2026-08-01 23:46:44 +0200

    fix copy&paste keybinds in neovide

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,3 +8,9 @@ vim.opt.smarttab = true
 vim.opt.signcolumn = "yes"
 
 vim.o.termguicolors = true
+
+if vim.g.neovide then
+    vim.keymap.set("v", "<sc-c>", '"+y', { noremap = true }) -- copy in visual mode
+    vim.keymap.set("i", "<sc-v>", "<C-r>+", { noremap = true }) -- paste in insert mode and stay in insert mode
+    vim.keymap.set("n", "<sc-v>", '"+p', { noremap = true }) -- paste in normal mode
+end
