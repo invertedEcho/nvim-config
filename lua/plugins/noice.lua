dcfb505e89e7d9dd66edfb6183a54672bb773bd2
Jakob Stechow <jakob.stechow@pm.me>
2026-09-25 17:24:09 +0200

    update

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,6 +1,7 @@
 return {
     "folke/noice.nvim",
     event = "VeryLazy",
+    enabled = false,
     dependencies = {
         -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
         "MunifTanjim/nui.nvim",
@@ -18,7 +19,7 @@ return {
             },
             -- you can enable a preset for easier configuration
             presets = {
-                bottom_search = true, -- use a classic bottom cmdline for search
+                bottom_search = false, -- use a classic bottom cmdline for search
                 command_palette = true, -- position the cmdline and popupmenu together
                 long_message_to_split = true, -- long messages will be sent to a split
                 inc_rename = false, -- enables an input dialog for inc-rename.nvim
@@ -28,6 +29,7 @@ return {
                 enabled = true,
             },
             cmdline = {
+                enabled = false,
                 view = "cmdline", -- default cmdline at bottom
             },
         })
---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,9 @@
+return {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+        input = { enabled = true },
+    },
+}
